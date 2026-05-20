7dc3c393eceb8cbb2bf4fd23c4538fe61037b143
xxdbddxx <sxxy4188@163.com>
2026-05-20 18:37:42 +0800

    feat: add I2S output driver for MAX98357A

--- main/main.c
+++ main/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "i2s_output.h"
 #include "peripherals.h"
 
 
 void app_main(void)
 {
+	i2s_output_init(NULL);
+	vTaskDelay(pdMS_TO_TICKS(500));
+	i2s_output_play_test_tone();
 	app_peripherals_init();
-    
 }
---
+++ main/peripherals/i2s_output.h
@@ -0,0 +1,45 @@
+#ifndef I2S_OUTPUT_H
+#define I2S_OUTPUT_H
+
+#include <stdint.h>
+#include "driver/gpio.h"
+#include "esp_err.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define I2S_OUTPUT_DEFAULT_SAMPLE_RATE_HZ  16000
+#define I2S_OUTPUT_DEFAULT_BITS_PER_SAMPLE 16
+#define I2S_OUTPUT_DEFAULT_TONE_HZ         440
+#define I2S_OUTPUT_DEFAULT_TONE_MS         1000
+
+#ifndef I2S_OUTPUT_DEFAULT_BCLK_GPIO
+#define I2S_OUTPUT_DEFAULT_BCLK_GPIO GPIO_NUM_4
+#endif
+
+#ifndef I2S_OUTPUT_DEFAULT_WS_GPIO
+#define I2S_OUTPUT_DEFAULT_WS_GPIO GPIO_NUM_5
+#endif
+
+#ifndef I2S_OUTPUT_DEFAULT_DOUT_GPIO
+#define I2S_OUTPUT_DEFAULT_DOUT_GPIO GPIO_NUM_6
+#endif
+
+typedef struct {
+    gpio_num_t bclk_gpio;
+    gpio_num_t ws_gpio;
+    gpio_num_t dout_gpio;
+    uint32_t sample_rate_hz;
+} i2s_output_config_t;
+
+esp_err_t i2s_output_init(const i2s_output_config_t *config);
+esp_err_t i2s_output_deinit(void);
+esp_err_t i2s_output_play_sine(uint32_t frequency_hz, uint32_t duration_ms);
+esp_err_t i2s_output_play_test_tone(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
